c3a1c686f2cb9f2d66a29fae1862381380504292
Jack McKew <jackmckew2@gmail.com>
2024-08-05 16:10:21 +1000

    Updates

--- src/portfolio.js
+++ src/portfolio.js
@@ -400,7 +400,7 @@ const experience = {
   title: "Experience",
   subtitle: "",
   description:
-    "I have worked with many evolving startups as Full Stack Software Engineer and Technical Writer. I have also worked with some well established companies mostly as a Electrical/Software Engineer. I love organising events and that is why I am also involved with many opensource communities as an organiser.",
+    "I have worked with many evolving startups as a Full Stack Software Engineer and Technical Writer. I have also worked with some well established companies mostly as a Electrical/Software Engineer. I love organising events and that is why I am also involved with many open source communities.",
   header_image_path: "experience.svg",
   sections: [
     {
@@ -522,7 +522,7 @@ const contactPageData = {
     title: "Contact Me",
     profile_image_path: "jm-photo.jpg",
     description:
-      "I am available on almost every social media. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
+      "I am available on almost every social media platform. You can message me, I will reply within 24 hours. I can help you with ML, AI, React, Android, Cloud and Opensource Development.",
   },
   blogSection: {
     title: "Blogs",
